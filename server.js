9aa2f5e48075d6768ae116600fa032a5c90ae193
Ryan Foote <footedesign@gmail.com>
2025-04-11 13:27:36 -0400

    Add basic hardening to server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const db = require('./db/database');
 const taskRoutes = require('./routes/tasks');
 
@@ -11,8 +13,19 @@ const app = express();
 const PORT = process.env.PORT || 4444;
 
 // --- Middleware ---
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// --- Rate Limiting ---
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+// Apply the rate limiting middleware to API calls only
+app.use('/api', apiLimiter);
 
 // --- API Routes ---
 app.use('/api', taskRoutes);
